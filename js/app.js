49349a4ef5a02fabd56d1db1784c307a6ee2123a
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-05-22 00:51:25 +0300

    hold and add winner score

--- js/app.js
+++ js/app.js
@@ -9,11 +9,12 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer;
+var score, roundScore, activePlayer,winnerScore;
 
 score = [0,0];
 roundScore = 0;
 activePlayer = 0;
+winnerScore = 10;
  
 
 document.querySelector('.dice').style.display = 'none';
@@ -43,19 +44,50 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore += dice;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = 0;
-        document.getElementById('current-1').textContent = 0;
 
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+        nextPalyer();
 
-        diceDom.style.display='none';
     }
 
 })
 
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add current score to GLobal score
+    score[activePlayer] += roundScore;
+    // Changing UI
+    document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+
+    if(score[activePlayer] >= winnerScore){
+        //change 'Player $' to 'Winner!'
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner !'
+        // Hide Dice
+        document.querySelector('.dice').style.display='none';
+        // Add winner class to make a 'Winner!' text a little shiny
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        // Remove active class when there is a winner
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+              
+    } else {
+
+   // next player
+    nextPalyer();
+    }
+
+})
+
+function nextPalyer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display='none';
+
+}
 
 
 
