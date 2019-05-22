689c51bbce3bf0373ae3598c1908ad2750706d1d
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-05-22 22:11:01 +0300

    initilize new game

--- js/app.js
+++ js/app.js
@@ -16,13 +16,7 @@ roundScore = 0;
 activePlayer = 0;
 winnerScore = 10;
  
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -78,6 +72,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 })
 
 function nextPalyer() {
+
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
     document.getElementById('current-0').textContent = 0;
@@ -89,6 +84,47 @@ function nextPalyer() {
     document.querySelector('.dice').style.display='none';
 
 }
+
+function init(){
+
+    score = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+
+    document.querySelector('#name-0').textContent = 'Player 1'
+    document.querySelector('#name-1').textContent = 'Player 2'
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
